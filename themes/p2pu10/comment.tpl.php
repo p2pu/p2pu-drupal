e36503c5b2f02bcd405a4f61e99a38ac9029c847
Charles Tanton <charles@parkroad.co.za>
2010-03-23 09:09:44 +0000

    Updated comments to now display comment title and new mark.

--- themes/p2pu10/comment.tpl.php
+++ themes/p2pu10/comment.tpl.php
@@ -1,5 +1,20 @@
 <div class="comment<?php print ($comment->new) ? ' comment-new' : ''; print ($comment->status == COMMENT_NOT_PUBLISHED) ? ' comment-unpublished' : ''; ?> clear-block">
+  
+  <?php if ($title): ?>
+    <h3 class="title">
+      <?php print $title; ?>
+      <?php if ($comment->new): ?>
+        <span class="new"><?php print $new; ?></span>
+      <?php endif; ?>
+    </h3>
+  <?php elseif ($comment->new): ?>
+    <div class="new"><?php print $new; ?></div>
+  <?php endif; ?>
 
+  <?php if ($unpublished): ?>
+    <div class="unpublished"><?php print t('Unpublished'); ?></div>
+  <?php endif; ?>
+  
   <div class="meta">
     <?php print $picture; ?>
     <span><?php print $submitted; ?></span>
